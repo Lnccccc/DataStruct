a720e9b3a7e1c8a9d4b069ca37b1f6236cba8b2c
lnccccc <601452185@qq.com>
2019-03-27 18:01:50 +0800

    '单向链表'

---
+++ LinkList.py
@@ -0,0 +1,13 @@
+class Node():
+    def __init__(self,data=None,next=None):
+        self.node = {data:next}
+
+    def InitList(self):
+        node = self.node
+
+    def InsertElem(self,i,elem):
+        pass
+        
+
+
+
